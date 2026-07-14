9cd68ecdbf7cf9408e2000b922f57475f68d57fc
evely me <devtys0@gmail.com>
2026-07-14 16:20:30 +0800

    Create AIRich.js

---
+++ AIRich.js
@@ -0,0 +1,128 @@
+tunggu conn.relayMessage(m.chat, {
+    messageContextInfo: {
+        deviceListMetadata: {},
+        deviceListMetadataVersion: 2,
+        botMetadata: {
+            pesanDisclaimerTeks: "Gweh",
+            richResponseSourcesMetadata: {
+                sumber: [
+                    {
+                        penyedia: "NIXEL",
+                        thumbnailCDNURL: "https://cdn.ornzora.eu.cc/d6b36500-3b7e-49ee-9123-52bb1bf106be-FIORA.jpg",
+                        URL penyedia sumber: "https://fiora.nixel.my.id/",
+                        sourceQuery: "",
+                        faviconCDNURL: "https://cdn.ornzora.eu.cc/4d2905ce-3707-4ec0-998a-68a3d851629f-FIORA.jpg",
+                        Nomor kutipan: 1,
+                        Judul sumber: "Nixel"
+                    },
+                    {
+                        penyedia: "NIXEL",
+                        thumbnailCDNURL: "https://cdn.ornzora.eu.cc/d6b36500-3b7e-49ee-9123-52bb1bf106be-FIORA.jpg",
+                        URL penyedia sumber: "https://fiora.nixel.my.id/",
+                        sourceQuery: "",
+                        faviconCDNURL: "https://cdn.ornzora.eu.cc/4d2905ce-3707-4ec0-998a-68a3d851629f-FIORA.jpg",
+                        Nomor kutipan: 2,
+                        Judul sumber: "Nixel"
+                    }
+                ]
+            }
+        }
+    },
+    botForwardedMessage: {
+        pesan: {
+            richResponseMessage: {
+                Jenis pesan: 1,
+                subpesan: [
+                    {
+                        Jenis pesan: 2,
+                        Teks pesan: ""
+                    },
+                    {
+                        Jenis pesan: 5,
+                        kodeMetadata: {
+                            Bahasa kode: "javascript",
+                            Blok kode: [
+                                { codeContent: "class", highlightType: 1 },
+                                { codeContent: " Nixel {\n\t", highlightType: 0 },
+                                { codeContent: "static", highlightType: 1 },
+                                { codeContent: " ", highlightType: 0 },
+                                { codeContent: "hello", highlightType: 2 },
+                                { codeContent: "() {\n\t\t", highlightType: 0 },
+                                { codeContent: "return", highlightType: 1 },
+                                { codeContent: " ", highlightType: 0 },
+                                { codeContent: "'Hello World'", highlightType: 3 },
+                                { codeContent: ";\n\t}\n}", highlightType: 0 }
+                            ]
+                        }
+                    },
+                    {
+                        Jenis pesan: 4,
+                        metadata tabel: {
+                            judul: "",
+                            baris: [
+                                { item: ["Nama", "Peran"], isHeading: true },
+                                { item: ["Nixel", "Pengembang"] },
+                                { item: ["Fiora Sylvie", "Asisten"] }
+                            ]
+                        }
+                    },
+                    {
+                        Jenis pesan: 1,
+                        gridImageMetadata: {
+                            gridImageUrl: {
+                                imagePreviewUrl: "https://cdn.ornzora.eu.cc/d987ff9c-c16c-4f1e-a8d6-953e375f4aec-FIORA.jpg"
+                            },
+                            URL gambar: [
+                                {
+                                    imagePreviewUrl: "https://cdn.ornzora.eu.cc/d987ff9c-c16c-4f1e-a8d6-953e375f4aec-FIORA.jpg",
+                                    imageHighResUrl: "https://cdn.ornzora.eu.cc/d987ff9c-c16c-4f1e-a8d6-953e375f4aec-FIORA.jpg",
+                                    URL sumber: "https://fiora.nixel.my.id/"
+                                },
+                                {
+                                    imagePreviewUrl: "https://cdn.ornzora.eu.cc/db9578dd-01e4-47ba-8a14-4c20e2aa4f52-FIORA.jpg",
+                                    imageHighResUrl: "https://cdn.ornzora.eu.cc/db9578dd-01e4-47ba-8a14-4c20e2aa4f52-FIORA.jpg",
+                                    URL sumber: "https://fiora.nixel.my.id/"
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        Jenis pesan: 9,
+                        contentItemsMetadata: {
+                            Tipe konten: 1,
+                            Metadata item: [
+                                {
+                                    reelItem: {
+                                        judul: "Nixel",
+                                        profileIconUrl: "https://cdn.ornzora.eu.cc/4d2905ce-3707-4ec0-998a-68a3d851629f-FIORA.jpg",
+                                        thumbnailUrl: "https://cdn.ornzora.eu.cc/d6b36500-3b7e-49ee-9123-52bb1bf106be-FIORA.jpg",
+                                        URL video: "https://fiora.nixel.my.id/"
+                                    }
+                                },
+                                {
+                                    reelItem: {
+                                        judul: "Nixel",
+                                        profileIconUrl: "https://cdn.ornzora.eu.cc/4d2905ce-3707-4ec0-998a-68a3d851629f-FIORA.jpg",
+                                        thumbnailUrl: "https://cdn.ornzora.eu.cc/fb402a04-3f96-49d1-b4e2-faebbbd4a22c-FIORA.jpg",
+                                        URL video: "https://fiora.nixel.my.id/"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ],
+                Respons terpadu: {
+                    data:"="="="
+        },
+        contextInfo: {
+                    Skor penerusan: 1,
+                    isForwarded: true,
+                    forwardedAiBotMessageInfo: {
+                        botJid: "0@bot"
+                    },
+                    Asal: 4
+                }
+            }
+        }
+    }
+}, {});
